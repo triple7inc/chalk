fe4c1ca95e06bea0366da76cb18166245d85f859
triple7inc <34259253+triple7inc@users.noreply.github.com>
2025-02-02 11:39:18 +0200

    chalk

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+setup(
+    name="chalk",
+    version="0.1",
+    author="triple7inc",
+    packages=find_packages(),
+    description="A simple package to change text color in CMD prompt using ANSI escape codes",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    url="https://github.com/triple7inc/chalk",
+    license_files=("LICENSE"),
+    install_requires=[],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=2.0"
+)
